6d37ab19397cddda275b80ce41a3ec7000056f8e
GroundedWren <120978593+GroundedWren@users.noreply.github.com>
2022-12-18 16:10:12 -0600

    Reupload with index

---
+++ scripts/Pages/Index.js
@@ -0,0 +1,15 @@
+﻿/**
+ * Namespace for index.html
+ */
+registerNamespace("Pages.Index", function (ns)
+{
+
+});
+
+/**
+ * Code to be called when the window first loads
+ */
+window.onload = () =>
+{
+
+};
