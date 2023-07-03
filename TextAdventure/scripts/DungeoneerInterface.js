ee519f4fd8c724ba2465ca93b4bae7388af06cbc
GroundedWren <120978593+GroundedWren@users.noreply.github.com>
2023-07-02 22:23:57 -0500

    Add files via upload

--- TextAdventure/scripts/DungeoneerInterface.js
+++ TextAdventure/scripts/DungeoneerInterface.js
@@ -6,6 +6,70 @@ registerNamespace("Pages.DungeoneerInterface", function (ns)
 	//The Meta Page Control
 	ns.MetaControl = null;
 	ns.InputConsole = null;
+
+	//#region File I/O
+	ns.saveToFile = () =>
+	{
+		Common.FileLib.saveJSONFile(
+			{ text: "this is text", num: 5 },
+			"New Save",
+			['.save', '.json']
+		);
+	};
+
+	ns.__shouldApplySave = false;
+	ns.saveUploaded = (filename, saveObj) =>
+	{
+		Common.Controls.Popups.showModal(
+			"Import Save",
+			`<h2>${filename}</h2>`
+			+ `<p>If you continue, any current data will be overwritten. Continue?</p>`
+			+ `<button style="float: right; height: 25px; margin-left: 5px;" onclick="Pages.DungeoneerInterface.__uploadSaveModalAccepted()">`
+			+ `Yes</button>`
+			+ `<button style="float: right; height: 25px;" onclick="Pages.DungeoneerInterface.__uploadSaveModalRejected()">`
+			+ `Never mind</button>`,
+			undefined,
+			() =>
+			{
+				if (ns.__shouldApplySave)
+				{
+					ns.Data = saveObj;
+					ns.applySaveData();
+				}
+				ns.__shouldApplySave = false;
+			}
+		);
+	};
+	ns.__uploadSaveModalAccepted = () =>
+	{
+		ns.__shouldApplySave = true;
+		Common.Controls.Popups.hideModal();
+		
+	};
+	ns.__uploadSaveModalRejected = () =>
+	{
+		ns.__shouldApplySave = false;
+		document.getElementById("saveUpload").value = null;
+		Common.Controls.Popups.hideModal();
+	};
+
+	ns.importFreshSave = (filename, path) =>
+	{
+		Common.FileLib.loadJSONFileFromDirectory(path).then(saveObj =>
+		{
+			if (!saveObj) { return; }
+			ns.saveUploaded(filename, saveObj);
+		});
+	};
+	//#endregion
+
+	ns.applySaveData = function ()
+	{
+		document.getElementById("currentGameTableBody").innerHTML = Object.keys(ns.Data.Meta).map(
+			key => `<tr><td>${key}</td><td>${ns.Data.Meta[key]}</td></tr>`
+		).join("");
+		document.getElementById("currentGameMetaCard").classList.remove("hidden");
+	};
 });
 
 /**
@@ -40,6 +104,15 @@ window.onload = () =>
 		"No Tab Selected",
 		"Meta Pane"
 	);
+
+	const saveUploadEl = document.getElementById("saveUpload");
+	saveUploadEl.addEventListener('change', (changeEv) =>
+	{
+		Common.FileLib.parseJSONFile(
+			changeEv.target.files[0],
+			Common.fcd(Pages.DungeoneerInterface, Pages.DungeoneerInterface.saveUploaded, [changeEv.target.files[0]])
+		);
+	});
 	//#endregion
 
 	//#region Console
